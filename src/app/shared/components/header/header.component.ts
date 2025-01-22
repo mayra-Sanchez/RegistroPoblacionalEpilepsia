f9e3177c1f95e94215015806898a0327f4e28f13
DylanFarkas <dylan.farkas@correounivalle.edu.co>
2025-01-21 23:35:13 -0500

    start guide

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,25 +7,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  isModalVisible: boolean = false;
+  isModalVisible: boolean = false;  // Controla si el modal está visible
+  modalType: string = '';  // Define el tipo de modal (login o help)
+  currentHelpSection: string = '';  // Controla la sección actual del manual
 
   constructor(private router: Router) {}
 
-  openModal(): void {
-    this.isModalVisible = true;
+  // Abre el modal de acuerdo al tipo
+  openModal(modalType: string): void {
+    this.modalType = modalType;  // Define el tipo del modal
+    this.isModalVisible = true;  // Muestra el modal
+    this.currentHelpSection = '';  // Resetea la sección de la guía
   }
 
+  // Cierra el modal
   closeModal(): void {
-    this.isModalVisible = false;
+    this.isModalVisible = false;  // Oculta el modal
+    this.modalType = '';  // Resetea el tipo de modal
   }
 
+  // Navegar a la ruta correspondiente
   navigateTo(route: string): void {
-    this.isModalVisible = false; 
+    this.isModalVisible = false;  // Cierra el modal al navegar
     if (route === '') {
       this.router.navigate(['/']);
     } else {
-      this.router.navigate([`/${route}`]); 
+      this.router.navigate([`/${route}`]);
     }
   }
-  
+
+  // Mostrar la sección correspondiente de la guía
+  showHelpSection(section: string): void {
+    this.currentHelpSection = section;  // Establece la sección activa de la guía
+  }
 }
